5882b04735d848896013d8b38fb931a609c3d7c8
VarunChowdary0 <saivarunchowdarypoludasu4248@gmail.com>
2025-01-29 10:01:11 +0530

    scdc

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -14,7 +14,7 @@ const Introduction: React.FC = () => {
 
   return (
     <div className='min-w-screen min-h-screen bg-black/0 flex max-sm:flex-col max-md:flex-col'>
-      <div className='_left w-[50%] max-md:w-full px-24 max-sm:px-5 max-sm:py-10 py-28 min-h-full bg-blue-400/0 max-sm:w-full max-sm:h-fit mt-20 max-md:pb-2 max-md:mt-0 max-md:pt-10 max-sm:pb-4'>
+      <div className='_left w-[50%] max-md:w-full px-24 max-sm:px-5 max-sm:py-10 py-28 min-h-full bg-blue-400/0 max-sm:w-full max-sm:h-fit mt-[9vh] max-md:pb-2 max-md:mt-0 max-md:pt-10 max-sm:pb-4'>
         <div className='flex h-fit w-full gap-16 max-sm:gap-3 max-sm:flex-col-reverse items-center justify-center'>
           <div className='w-fit h-fit flex gap-16 items-center py-7 max-w-screen justify-around mt-1 flex-col max-sm:py-3 max-sm:gap-9 bg-black/0 px-3 rounded-md max-sm:flex-row z-0'>
             <a target='_blank' className='scale-[1.75]' href="https://www.linkedin.com/in/sai-varun-chowdary-poludasu-908051259/">
@@ -74,9 +74,11 @@ const Introduction: React.FC = () => {
       <div className='_rigth w-[50%] max-md:w-full min-h-full bg-red-400/0 max-sm:w-full max-sm:h-fit flex'>
         <div className='max-sm:hidden select-none h-fit w-fit mt-20 max-sm:mt-0 bg-black/0 transition-all animate-random-move'>
           <img className='' src='/2f533018fa4725ec75a26c1fab07cb8a.png' alt='' />
+          {/* <img className='' src='/me.png' alt='' /> */}
         </div>
         <div className='select-none hidden max-sm:block h-fit w-fit mt-20 max-sm:mt-0 bg-black/0 transition-all animate-random-move-sm'>
           <img className='' src='/2f533018fa4725ec75a26c1fab07cb8a.png' alt='' />
+          {/* <img className='' src='/me.png' alt='' /> */}
         </div>
       </div>
     </div>
