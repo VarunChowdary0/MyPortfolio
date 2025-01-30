eec61b0af23fc0df0f482186d366f1027bd44bf5
VarunChowdary0 <saivarunchowdarypoludasu4248@gmail.com>
2025-01-30 13:29:08 +0530

    setter

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -15,10 +15,12 @@ const Introduction: React.FC = () => {
 
   return (
     <div className='min-w-screen h-[calc(100vh-40px)] bg-black/0 flex max-sm:flex-col
-     max-md:overflow-y-auto 
-     max-sm:pt-0 max-sm:justify-start max-sm:items-start pt-[2vh] overflow-hidden max-md:flex-col'>
-      <div className='_left w-[50%] max-md:w-full px-24 max-sm:px-5 max-sm:py-10 py-28 h-fit 
-      bg-blue-400/0 max-sm:w-full max-sm:h-fit mt-[9vh] max-md:pb-2 max-md:mt-0 max-md:pt-10 max-sm:pb-4'>
+    max-md:overflow-y-auto py-[8vh] 
+    max-md:items-start max-md:justify-start 
+    max-sm:pt-0 max-sm:justify-start max-sm:items-start 
+    pt-[vh] overflow-hidden max-md:flex-col'>
+      <div className='_left w-[50%] max-md:pt-20 max-md:w-full px-24 max-sm:px-5 max-sm:py-10 py-28 h-fit 
+      bg-blue-400/0 max-sm:w-full max-sm:h-fit mt-[9vh] max-md:pb-2 max-md:mt-0  max-sm:pb-4'>
         <div className='flex h-fit w-full gap-16 max-sm:gap-3 max-sm:flex-col-reverse items-center justify-center'>
           <div className='w-fit h-fit flex gap-16 items-center py-7 max-w-screen justify-around mt-1 flex-col max-sm:py-3 max-sm:gap-9 bg-black/0 px-3 rounded-md max-sm:flex-row z-0'>
             <a target='_blank' className='scale-[1.75]' href="https://www.linkedin.com/in/sai-varun-chowdary-poludasu-908051259/">
